f497d7aec322f180f87f22f9852aa2e6052be342
Gabrysia <gabrielapiszcz@gmail.com>
2018-05-04 20:56:04 +0100

    Maven rebuild

--- src/main/java/com/kwietniewski/clientcebs/MainApp.java
+++ src/main/java/com/kwietniewski/clientcebs/MainApp.java
@@ -63,11 +63,12 @@ public class MainApp extends Application {
     
     public int login(String email, String password) throws IOException{
         String url = "http://localhost:8181/api/customers/login?";
-        post(url, email, password);
+        String urlParameters = "email="+email+"&password="+password;
+        post(url, urlParameters);
         return 1;
     }
    
-    public void post(String url, String email, String password) throws MalformedURLException, ProtocolException, IOException{
+    public void post(String url, String urlParameters) throws MalformedURLException, ProtocolException, IOException{
         
 	URL obj = new URL(url);
 	HttpURLConnection con = (HttpURLConnection) obj.openConnection();
@@ -76,8 +77,6 @@ public class MainApp extends Application {
 	con.setRequestMethod("POST");
 	con.setRequestProperty("User-Agent", USER_AGENT);
 	con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-
-	String urlParameters = "email="+email+"&password="+password;
 		
 	// Send post request
 	con.setDoOutput(true);
